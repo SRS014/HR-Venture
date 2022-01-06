21094ae8a95a076446ca8effd4090bea4301014a
S R Sohan <srsohan014@gmail.com>
2022-01-06 11:26:30 +0600

    Upload HR Venture All Codes

---
+++ class-01/program.py
@@ -0,0 +1,19 @@
+
+#First Code
+
+print("Choose the operation, which operation do you perform?\n1.Addition.\n2.Subtraction.\n3.Multiplication.\n4.Division.")
+operation = int(input("Enter which operation do you want: "))
+
+num1 = int(input("Enter the Number1: ")) 
+num2 = int(input("Enter the Number2: "))
+
+if(operation==1):    
+    result = num1+num2
+elif(operation==2):    
+    result = num1-num2
+elif(operation==3):    
+    result = num1*num2
+elif(operation==4):    
+    result = num1/num2
+
+print("The Result is:", result)
